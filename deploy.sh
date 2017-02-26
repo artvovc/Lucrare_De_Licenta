ba005c627647bf5b72dedd2ac9a6bd3a6eeeef55
Mihail <urmanschi.mihail@gmail.com>
2017-02-26 13:37:51 +0200

    fixed deploy sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ rm -rf nnServer-WebServer/target
 #make clean install of server
 mvn clean install
 # shutdown remote server and remove war file
-ssh root@207.154.210.175 '/home/artvovc/tomcat/apache-tomcat-7.0.70/bin/./shutdown.sh ;rm -f /home/artvovc/tomcat/apache-tomcat-7.0.70/webapps/*war'  
+ssh root@207.154.210.175 '/home/artvovc/tomcat/apache-tomcat*/bin/./shutdown.sh ;rm -f /home/artvovc/tomcat/apache-tomcat*/webapps/*war'  
 # copy war serv on root
 cp nnServer-WebServer/target/*war serv.war
 #send war folder to remote serv
-scp serv.war root@207.154.210.175:/home/artvovc/tomcat/apache-tomcat-7.0.70/webapps/
+scp serv.war root@207.154.210.175:/home/artvovc/tomcat/apache-tomcat*/webapps/
 # start remote server
-ssh root@207.154.210.175 '/home/artvovc/tomcat/apache-tomcat-7.0.70/bin/./startup.sh '  
+ssh root@207.154.210.175 '/home/artvovc/tomcat/apache-tomcat*/bin/./startup.sh '  
