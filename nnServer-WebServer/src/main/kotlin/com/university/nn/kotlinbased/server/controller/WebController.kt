0ea30c09cdbb6ce19023e723a021436405c8ace5
artvovc <mortivsfelya@gmail.com>
2017-02-27 20:34:48 +0200

    improved deploy script created by Mihail

--- nnServer-WebServer/src/main/kotlin/com/university/nn/kotlinbased/server/controller/WebController.kt
+++ nnServer-WebServer/src/main/kotlin/com/university/nn/kotlinbased/server/controller/WebController.kt
@@ -1,6 +1,5 @@
 package com.university.nn.kotlinbased.server.controller
 
-import com.university.nn.kotlinbased.db.model.NNUser
 import com.university.nn.kotlinbased.db.request.RequestFeeds
 import com.university.nn.kotlinbased.db.request.RequestSearch
 import com.university.nn.kotlinbased.db.response.Pagination
@@ -8,14 +7,12 @@ import com.university.nn.kotlinbased.services.FeedService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.servlet.ModelAndView
 
 @Controller
